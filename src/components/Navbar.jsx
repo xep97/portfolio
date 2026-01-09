f0c48b1a3642a030cc6a1dd04660c3f329fdafed
xep97 <pettersen.egil@gmail.com>
2026-01-09 15:01:42 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+        <nav>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </nav>
+    </>
+  )
+}
